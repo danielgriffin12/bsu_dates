10960b91c778cf2e9355a085ae4466b37733c16d
Alex Kluber <ajkluber@bsu.edu>
2020-04-23 15:37:16 -0400

    refactor: create DataFrame instead of dictionary

--- __init__.py
+++ __init__.py
@@ -34,23 +34,23 @@ class BSUCalendar(object):
             "Independence Day",
         )
 
-        self.tags = {
-            "Registration": (
-                "Late Registration Start",
-                "Late Registration End",
-                "Withdraw Deadline",
-            ),
-            "Instruction": (
-                "Classes Start",
-                "Classes End",
-                "Break Start",
-                "Break End",
-                "Finals Start",
-                "Finals End",
-                "Final Grades Due",
-            ),
-            "Holidays": self.holidays,
-        }
+        # self.tags = {
+        #     "Registration": (
+        #         "Late Registration Start",
+        #         "Late Registration End",
+        #         "Withdraw Deadline",
+        #     ),
+        #     "Instruction": (
+        #         "Classes Start",
+        #         "Classes End",
+        #         "Break Start",
+        #         "Break End",
+        #         "Finals Start",
+        #         "Finals End",
+        #         "Final Grades Due",
+        #     ),
+        #     "Holidays": self.holidays,
+        # }
         # TODO: "Midterm Grades Due", for Instruction
 
         if start_year < 2012:
@@ -82,105 +82,75 @@ class BSUCalendar(object):
         fall_break_start = self._fall_break_start(**timeframe)
         spring_withdraw = self._spring_withdraw_deadline(**timeframe)
 
-        # should return a dataframe with columns:
-        # Term, Year, Semester, DateName, Date, Tags
-
-        self.dates = {}
+        # Creates dataframe with columns:
+        #   Term, Year, Semester, DateName, Date, Tags
+        dates_df = []
         for yr in self.years:
-            # dues for this academic year
+            # Note: **academic** year
             yr_dates = {}
 
             # fall term
             f_start = next(fall_start)
             f_end = f_start + term_length
 
-            f_break_start = next(fall_break_start)
-            f_break_end = self._add_days(f_break_start, 1)
-            f_withdraw_end = self._add_days(f_break_end, 1)
-
-            f_thank_break_start = self._add_days(next(thanksgiving), -1)
-            f_thank_break_end = self._add_days(f_thank_break_start, 2)
-
-            yr_dates["Fall Term Start"] = f_start
-            yr_dates["Fall Classes Start"] = f_start
-            yr_dates["Fall Late Registration Start"] = f_start
-            yr_dates["Fall Late Registration End"] = self._add_days(f_start, 4)
-            yr_dates["Fall Break Start"] = f_break_start
-            yr_dates["Fall Break End"] = f_break_end
-            yr_dates["Fall Withdraw Deadline"] = f_withdraw_end
-            yr_dates["Thanksgiving Break Start"] = f_thank_break_start
-            yr_dates["Thanksgiving Break End"] = f_thank_break_end
-            yr_dates["Labor Day"] = next(labor_day)
-            yr_dates["Fall Classes End"] = self._add_days(f_end, -4)
-            yr_dates["Fall Finals Start"] = self._add_days(f_end, -3)
-            yr_dates["Fall Finals End"] = f_end
-            yr_dates["Fall Term End"] = f_end
-            yr_dates["Fall Final Grades Due"] = self._add_days(f_end, 3)
+            dates_df.append(
+                self._get_fall_dates(
+                    yr, f_start, f_end, fall_break_start, labor_day, thanksgiving,
+                )
+            )
 
             # spring term
             sp_start = self._add_days(f_end, xmas_break)
             sp_end = self._add_days(sp_start, term_length)
-            if yr == 2013:
-                sp_break_start = sp_start + datetime.timedelta(weeks=9)
-            else:
-                sp_break_start = sp_start + datetime.timedelta(weeks=8)
 
-            yr_dates["Spring Term Start"] = sp_start
-            yr_dates["Spring Classes Start"] = sp_start
-            yr_dates["Spring Break Start"] = sp_break_start
-            yr_dates["Spring Break End"] = self._add_days(sp_break_start, 4)
-            yr_dates["Spring Withdraw Deadline"] = next(spring_withdraw)
-            yr_dates["Spring Classes End"] = self._add_days(sp_end, -4)
-            yr_dates["Spring Finals Start"] = self._add_days(sp_end, -3)
-            yr_dates["Spring Finals End"] = sp_end
-            yr_dates["Spring Term End"] = sp_end
-            yr_dates["Spring Final Grades Due"] = self._add_days(sp_end, 3)
+            dates_df.append(
+                self._get_spring_dates(yr, sp_start, sp_end, mlk_day, spring_withdraw)
+            )
 
             # summer term
             sm_start = self._add_days(sp_end, 10)
             sm_end = self._add_days(sp_start, sum_term_length)
-
-            yr_dates["Summer Term Start"] = sm_start
-            yr_dates["Independenc Day"] = next(independence_day)
-            # missing dates here
-            yr_dates["Summer Term End"] = sm_end
-            yr_dates["Summer Final Grades Due"] = self._add_days(sm_end, 3)
-
-            self.dates[yr] = yr_dates
-
-    def date_in_term(self, term, field):
-        """Get a date in a term"""
-        if isinstance(term, str):
-            term = int(term)
-        yr = int(round(term, -2) / 100)
-        return self.dates[yr][field]
-
-    def dates_by_tag(self, tag):
-        """Get all dates for a tag"""
-        yr = int(round(term, -2) / 100)
-        sem = self.int_to_sem_code(term - round(term, -2))
-        if tag in self.tags:
-            fields = self.tags[tag]
-            if tag == "Holidays":
-                vals = {}
-                for hol in self.holidays:
-                    if hol == "Thanksgiving Break":
-                        hol_d = self.get_holiday(hol.rstrip(" Break"))
-                        vals[hol + " Start"] = self._add_days(hol_d, -1)
-                        vals[hol + " End"] = self._add_days(hol_d, 1)
-                    else:
-                        vals[hol] = self.get_holiday(hol)
-            else:
-                vals = {
-                    f"{sem} {fld}": self.dates[f"{sem} {fld}"]
-                    for sem in self.semesters
-                    for fld in fields
-                }
-            return vals
-        else:
-            ValueError(
-                f"{tag} not valid. Choose from: {list(self.tags.keys()).__str__()}"
+            dates_df.append(
+                self._get_summer_dates(
+                    yr, sm_start, sm_end, memorial_day, independence_day
+                )
             )
+
+        self.dates_df = pd.concat(dates_df)
+
+    # def date_in_term(self, term, field):
+    #     """Get a date in a term"""
+    #     if isinstance(term, str):
+    #         term = int(term)
+    #     yr = int(round(term, -2) / 100)
+    #     return self.dates[yr][field]
+
+    # def dates_by_tag(self, tag):
+    #     """Get all dates for a tag"""
+    #     yr = int(round(term, -2) / 100)
+    #     sem = self.int_to_sem_code(term - round(term, -2))
+    #     if tag in self.tags:
+    #         fields = self.tags[tag]
+    #         if tag == "Holidays":
+    #             vals = {}
+    #             for hol in self.holidays:
+    #                 if hol == "Thanksgiving Break":
+    #                     hol_d = self.get_holiday(hol.rstrip(" Break"))
+    #                     vals[hol + " Start"] = self._add_days(hol_d, -1)
+    #                     vals[hol + " End"] = self._add_days(hol_d, 1)
+    #                 else:
+    #                     vals[hol] = self.get_holiday(hol)
+    #         else:
+    #             vals = {
+    #                 f"{sem} {fld}": self.dates[f"{sem} {fld}"]
+    #                 for sem in self.semesters
+    #                 for fld in fields
+    #             }
+    #         return vals
+    #     else:
+    #         ValueError(
+    #             f"{tag} not valid. Choose from: {list(self.tags.keys()).__str__()}"
+    #         )
 
     def get_holiday(self, holiday, ac_year):
         """Return the date of a holiday for a given academic year"""
@@ -192,6 +162,88 @@ class BSUCalendar(object):
             return list(gen_holiday(dtstart=datetime.date(ac_year, 1, 1), count=1))[0]
         else:
             raise ValueError(f"Holiday {holiday} not recognized")
+
+    def _get_fall_dates(
+        self, yr, f_start, f_end, fall_break_start, labor_day, thanksgiving
+    ):
+        """Fall term important dates"""
+        f_term = 100 * yr + 10
+        f_break_start = next(fall_break_start)
+        f_break_end = self._add_days(f_break_start, 1)
+        f_withdraw_end = self._add_days(f_break_end, 1)
+        f_labor = next(labor_day)
+        f_thank = next(thanksgiving)
+        f_thank_break_start = self._add_days(f_thank, -1)
+        f_thank_break_end = self._add_days(f_thank_break_start, 2)
+
+        # DateName, Date, Tags
+        rows = [
+            ["Term Start", f_start, "Instruction"],
+            ["Classes Start", f_start, "Instruction"],
+            ["Late Registration Start", f_start, "Registration"],
+            ["Late Registration End", self._add_days(f_start, 4), "Registration"],
+            ["Break Start", f_break_start, "Instruction"],
+            ["Break End", f_break_end, "Instruction"],
+            ["Withdraw Deadline", f_withdraw_end, "Registration"],
+            ["Thanksgiving Break Start", self._add_days(f_thank, -1), "Instruction"],
+            ["Thanksgiving", f_thank, "Instruction,Holiday"],
+            ["Thanksgiving Break End", self._add_days(f_thank, 1), "Instruction"],
+            ["Labor Day", f_labor, "Instruction,Holiday"],
+            ["Classes End", self._add_days(f_end, -4), "Instruction"],
+            ["Finals Start", self._add_days(f_end, -3), "Instruction"],
+            ["Finals End", f_end, "Instruction",],
+            ["Term End", self._add_days(f_end, -3), "Instruction"],
+            ["Final Grades Due", self._add_days(f_end, 3), "Instruction"],
+        ]
+
+        return self._make_semester_df(yr, "Fall", rows)
+
+    def _get_spring_dates(self, yr, sp_start, sp_end, mlk_day, spring_withdraw):
+        """Spring term important dates"""
+        if yr == 2013:
+            sp_break_start = sp_start + datetime.timedelta(weeks=9)
+        else:
+            sp_break_start = sp_start + datetime.timedelta(weeks=8)
+
+        rows = [
+            ["Spring Term Start", sp_start, "Instruction"],
+            ["Spring Classes Start", sp_start, "Instruction"],
+            ["MLK Day", next(mlk_day), "Instruction,Holiday"],
+            ["Spring Break Start", sp_break_start, "Instruction"],
+            ["Spring Break End", self._add_days(sp_break_start, 4), "Instruction"],
+            ["Spring Withdraw Deadline", next(spring_withdraw), "Registration"],
+            ["Spring Classes End", self._add_days(sp_end, -4), "Instruction"],
+            ["Spring Finals Start", self._add_days(sp_end, -3), "Instruction"],
+            ["Spring Finals End", sp_end, "Instruction"],
+            ["Spring Term End", sp_end, "Instruction"],
+            ["Spring Final Grades Due", self._add_days(sp_end, 3), "Instruction"],
+        ]
+
+        return self._make_semester_df(yr, "Spring", rows)
+
+    def _get_summer_dates(self, yr, sm_start, sm_end, memorial_day, independence_day):
+        """Summer term important dates"""
+        rows = [
+            ["Summer Term Start", sm_start, "Instruction"],
+            ["Independence Day", next(independence_day), "Instruction,Holiday"],
+            ["Summer Term End", sm_end, "Instruction"],
+            ["Summer Final Grades Due", self._add_days(sm_end, 3), "Instruction"],
+        ]
+        return self._make_semester_df(yr, "Summer", rows)
+
+    def _make_semester_df(self, yr, semester, rows):
+        """Take list of semester date data and return DataFrame"""
+        term = 100 * yr + self.sem_code_to_int(semester)
+        n_rows = len(rows)
+        data = {
+            "Term": n_rows * [term],
+            "Semester": n_rows * [semester],
+            "Year": n_rows * [yr],
+            "DateName": [r[0] for r in rows],
+            "Date": [r[1] for r in rows],
+            "Tags": [r[2] for r in rows],
+        }
+        return pd.DataFrame(data=data)
 
     def _set_logger(self, level):
         """Create logger with level"""
